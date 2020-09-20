b046e64a4eb244fbc1c65f2f4ad84b8294082942
uzytkownik <uzytkownik@kursGIT.pl>
2020-09-20 10:37:00 +0200

    Dodanie TabliczkaMnozenia i Zadanie1/Day2

--- src/sda/basic/java/day1/TabliczkaMnozenia.java
+++ src/sda/basic/java/day1/TabliczkaMnozenia.java
@@ -1,2 +1,14 @@
-package sda.basic.java.day1;public class TabliczkaMnozenia {
+package sda.basic.java.day1;
+
+public class TabliczkaMnozenia {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            for (int j = 1; j <= 10 ; j++) {
+                System.out.print("\t" + i * j);
+            }
+            System.out.println();
+
+        }
+    }
 }
---
+++ src/sda/basic/java/day2/Questions.java
@@ -0,0 +1,8 @@
+package sda.basic.java.day2;
+
+public class Questions {
+
+    public static void main(String[] args) {
+
+    }
+}
